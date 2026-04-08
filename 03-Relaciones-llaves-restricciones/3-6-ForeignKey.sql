1d187ec51f281fc41633ae457971ae47b68973f0
xDennis20 <dennis_6@outlook.com>
2026-04-08 07:59:59 -0500

    feat: ForeignKey

---
+++ 03-Relaciones-llaves-restricciones/3-6-ForeignKey.sql
@@ -0,0 +1,9 @@
+ALTER TABLE city
+    ADD CONSTRAINT fk_country_code
+    FOREIGN KEY (countrycode)
+    REFERENCES country(code);
+
+ALTER TABLE countrylanguage
+    ADD CONSTRAINT fk_country_code
+    FOREIGN KEY (countrycode)
+    REFERENCES country(code);
