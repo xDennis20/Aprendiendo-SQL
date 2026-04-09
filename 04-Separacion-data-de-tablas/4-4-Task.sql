00a1288303e46c7cfb66bffff7108696ab9ed7f5
xDennis20 <dennis_6@outlook.com>
2026-04-09 10:38:20 -0500

    feat: Section task.

--- 04-Separacion-data-de-tablas/4-4-Task.sql
+++ 04-Separacion-data-de-tablas/4-4-Task.sql
@@ -14,3 +14,35 @@ CREATE TABLE "public"."language"
     PRIMARY KEY ("code")
 );
 
+-- Crear una columna en countrylanguage
+ALTER TABLE countrylanguage
+    ADD COLUMN languagecode varchar(3);
+
+
+-- Empezar con el select para confirmar lo que vamos a actualizar
+INSERT INTO language (name)
+SELECT DISTINCT language
+FROM countrylanguage;
+
+SELECT a.language,
+       (SELECT b.code FROM language as b WHERE b.name = a.language)
+FROM countrylanguage as a;
+
+-- Actualizar todos los registros
+UPDATE countrylanguage as a
+SET languagecode = (SELECT b.code FROM language as b WHERE b.name = a.language);
+
+-- Cambiar tipo de dato en countrylanguage - languagecode por int4
+ALTER TABLE countrylanguage
+    ALTER COLUMN languagecode TYPE int4
+        USING languagecode::integer;
+
+-- Crear el forening key y constraints de no nulo el language_code
+ALTER TABLE countrylanguage
+    ADD CONSTRAINT fk_code_language
+        FOREIGN KEY (languagecode)
+            REFERENCES language (code);
+
+-- Revisar lo creado
+SELECT *
+FROM countrylanguage;
