3ed1a6453e8bd9387de2bffafdebd25e0ae61ac2
xDennis20 <dennis_6@outlook.com>
2026-04-12 09:28:46 -0500

    feat: Task-Count-Union

--- 05-Join-Unions/5-4-Task-Count-Union.sql
+++ 05-Join-Unions/5-4-Task-Count-Union.sql
@@ -17,3 +17,14 @@ SELECT count(c.name) as total,
        'America'
 FROM country as a
          INNER JOIN continent c on c.code = a.continent AND c.name LIKE '%America%';
+
+-- Solucion mas eficiente
+SELECT count(*) as total,
+       CASE
+           WHEN c.name LIKE '%America%' THEN 'America'
+           ELSE c.name
+           END as continente_maestro
+FROM country as a
+         INNER JOIN continent c on c.code = a.continent
+GROUP BY continente_maestro
+ORDER BY total ASC;
