8896ad3de0a727b952c30b44f8868734e9ca5c2c
xDennis20 <dennis_6@outlook.com>
2026-04-21 08:52:54 -0500

    feat: CTE Recursived, more exercesive

--- 08-Vistas-CTE/8-3-CTE-Recursive.sql
+++ 08-Vistas-CTE/8-3-CTE-Recursive.sql
@@ -10,7 +10,7 @@ WITH RECURSIVE countdown(val) as (
 )
 SELECT * FROM countdown;
 
-
+-- 2 Ejercicio
 WITH RECURSIVE increase(val) as (
     -- inicializacion -> Es decir que con que valores va empezar la recursividad
     -- Quiero que empieze con el valor de 1
@@ -21,9 +21,21 @@ WITH RECURSIVE increase(val) as (
 )
 SELECT * FROM increase;
 
+-- 3 Ejercicio
 WITH RECURSIVE multiplication(base, multiplicador, result) as (
     SELECT 5 as base , 1 as multiplicador, 5 as result
     UNION
     SELECT base, multiplicador + 1, base * (multiplicador + 1 )  FROM multiplication WHERE multiplicador < 10
 )
 SELECT * FROM multiplication;
+
+-- Ejercicio de empleados y jefes
+WITH RECURSIVE bosses as (
+    -- inicializacion
+    SELECT id, name, report_to FROM employees where id = 6
+    UNION
+    -- iteracion recursiva
+    SELECT a.id, a.name, a.report_to FROM employees as a
+                                              INNER JOIN bosses as b ON b.id = a.report_to
+)
+SELECT * FROM bosses;
