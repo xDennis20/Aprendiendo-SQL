5108a0d5034cee36d6e685766d6fb80511c77541
xDennis20 <dennis_6@outlook.com>
2026-04-21 06:09:49 -0500

    feat: View and View materialized

---
+++ 08-Vistas-CTE/8-1-View-and-View-materialized.sql
@@ -0,0 +1,29 @@
+CREATE MATERIALIZED VIEW  comments_per_weeks_mat AS
+(SELECT date_trunc('weeks', posts.created_at) as weeks,
+       count(DISTINCT posts.created_at) as total_posts,
+       sum(c.counter) as sum_claps
+FROM posts
+INNER JOIN claps c on posts.post_id = c.post_id
+GROUP BY weeks
+ORDER BY weeks DESC); -- Esto es una vista materializada,
+-- A diferencia de una vista normal este cuando lo creamos
+-- Este guarda los datos de ese momento como una tabla y cuando lo llamamos ya no ejecuta el query
+-- Por lo que lo hace mas eficiente al momento de hacer consultas.
+
+
+CREATE OR REPLACE VIEW  comments_per_weeks AS
+(SELECT date_trunc('weeks', posts.created_at) as weeks,
+        count(DISTINCT posts.created_at) as total_posts,
+        sum(c.counter) as sum_claps
+FROM posts
+         INNER JOIN claps c on posts.post_id = c.post_id
+GROUP BY weeks
+ORDER BY weeks DESC); -- Esto es una vista,
+-- sirve para guardar un query como si fuera una tabla pero no lo es ya que
+-- cada vez que lo llamamos ejecuta todo el query por lo que no es tan eficiente
+
+SELECT * FROM comments_per_weeks;
+
+SELECT * FROM comments_per_weeks_mat;
+
+REFRESH MATERIALIZED VIEW comments_per_weeks_mat; -- Esto sirve para actualizar los datos de la vista materializada
