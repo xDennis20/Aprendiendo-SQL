0713ce5e9347d86119ee907983370612eee46570
xDennis20 <dennis_6@outlook.com>
2026-04-23 08:05:32 -0500

    feat: functions

--- 09-Functions/9-1-Functions.sql
+++ 09-Functions/9-1-Functions.sql
@@ -1,9 +1,28 @@
-
-CREATE OR REPLACE FUNCTION greet_employe (employee_name VARCHAR)
+CREATE OR REPLACE FUNCTION greet_employe(employee_name VARCHAR)
     RETURNS VARCHAR
-AS $$
+AS
+$$
 BEGIN
     RETURN 'Hola ' || employee_name;
+end;
+$$
+    LANGUAGE plpgsql;
+
+-- Ejercicio 2
+CREATE OR REPLACE FUNCTION max_raise(empl_id INT)
+    RETURNS NUMERIC(8, 2)
+AS
+$$
+DECLARE
+    total_increase NUMERIC(8, 2);
+BEGIN
+    SELECT j.max_salary - salary
+    into total_increase
+    FROM employees
+             INNER JOIN public.jobs j on j.job_id = employees.job_id
+    WHERE employees.employee_id = empl_id;
+
+    RETURN total_increase;
 end;
 $$
     LANGUAGE plpgsql;
