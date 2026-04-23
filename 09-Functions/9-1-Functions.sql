a834a5dc0414e6407b8858f559699c487fd95ba3
xDennis20 <dennis_6@outlook.com>
2026-04-23 09:50:24 -0500

    feat: functions

--- 09-Functions/9-1-Functions.sql
+++ 09-Functions/9-1-Functions.sql
@@ -26,3 +26,75 @@ BEGIN
 end;
 $$
     LANGUAGE plpgsql;
+
+-- alternativa de la funcion max_raise
+
+CREATE OR REPLACE FUNCTION max_raise_2(empl_id INT)
+    RETURNS NUMERIC(8, 2)
+AS
+$$
+DECLARE
+    job_id_actual  INT;
+    total_increase NUMERIC(8, 2);
+    current_salary NUMERIC(8, 2);
+    job_max_salary NUMERIC(8, 2);
+
+BEGIN
+    -- Tomar el job_id y el salario del empleado
+    SELECT salary, job_id
+    into current_salary, job_id_actual
+    FROM employees
+    WHERE employee_id = empl_id;
+
+    -- Tomar el salario maximo del job
+    SELECT max_salary
+    into job_max_salary
+    FROM jobs
+    WHERE job_id = job_id_actual;
+
+    -- Operacion
+    total_increase = job_max_salary - current_salary;
+    IF (total_increase < 0 ) THEN
+        RAISE EXCEPTION 'Empleado con salario mas alto que el salario maximo: %', empl_id;
+    end if;
+
+    RETURN total_increase;
+end;
+$$
+    LANGUAGE plpgsql;
+
+-- Max_raise_2 con rowtype
+
+CREATE OR REPLACE FUNCTION max_raise_2_row(empl_id INT)
+    RETURNS NUMERIC(8, 2)
+AS
+$$
+DECLARE
+    row_employee employees%rowtype;
+    row_job jobs%rowtype;
+    total_increase NUMERIC(8,2);
+
+BEGIN
+    -- Tomar el job_id y el salario del empleado
+    SELECT *
+    INTO row_employee
+    FROM employees
+    WHERE employee_id = empl_id;
+
+    -- Tomar el salario maximo del job
+    SELECT *
+    into row_job
+    FROM jobs
+    WHERE job_id = row_employee.job_id;
+
+    -- Operacion
+    total_increase = row_job.max_salary - row_employee.salary;
+
+    IF (total_increase < 0 ) THEN
+        RAISE EXCEPTION 'Empleado con salario mas alto que el salario maximo: %, %', row_employee.employee_id, row_employee.first_name;
+    end if;
+
+    RETURN total_increase;
+end;
+$$
+    LANGUAGE plpgsql;
