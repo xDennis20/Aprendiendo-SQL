0162542ec72da9f6e56484e0cef8f8dc1dd908e3
xDennis20 <dennis_6@outlook.com>
2026-05-07 10:07:54 -0500

    feat: exercise-triggers

--- 11-Triggers/11-1-Triggers.sql
+++ 11-Triggers/11-1-Triggers.sql
@@ -35,9 +35,10 @@ $$
     LANGUAGE plpgsql;
 
 CREATE TRIGGER vigilar_salarios
-    BEFORE UPDATE ON employees
+    BEFORE UPDATE
+    ON employees
     FOR EACH ROW
-    EXECUTE PROCEDURE registrar_cambio_salario();
+EXECUTE PROCEDURE registrar_cambio_salario();
 
 /*
  Ejercicio 2:
@@ -53,7 +54,7 @@ CREATE OR REPLACE FUNCTION modificar_email_invalido()
 $$
 BEGIN
     NEW.email := lower(NEW.email);
-    IF ( NEW.email NOT LIKE '%@sqltutorial.org' ) THEN
+    IF (NEW.email NOT LIKE '%@sqltutorial.org') THEN
         NEW.email := NEW.email || '@sqltutorial.org';
     end if;
     RETURN NEW;
@@ -62,6 +63,54 @@ $$
     LANGUAGE plpgsql;
 
 CREATE OR REPLACE TRIGGER vigilar_email
-    BEFORE INSERT OR UPDATE ON employees
+    BEFORE INSERT OR UPDATE
+    ON employees
     FOR EACH ROW
-    EXECUTE PROCEDURE modificar_email_invalido();
+EXECUTE PROCEDURE modificar_email_invalido();
+
+/*
+ Ejercicio 3:
+ Objetivo: Usar un Trigger para hacer cruces de tablas y abortar una operación si viola una regla de negocio.
+Tablas a usar: employees, jobs
+ El Requerimiento:
+Ningún empleado puede ganar menos que el salario mínimo (min_salary) ni más que el salario máximo (max_salary) dictado por su puesto de trabajo (job_id).
+ */
+
+CREATE OR REPLACE FUNCTION verificar_salario()
+    RETURNS TRIGGER AS
+$$
+DECLARE
+    max_salary_job NUMERIC(8,2);
+    min_salary_job NUMERIC(8,2);
+BEGIN
+    RAISE NOTICE '¡EL TRIGGER DESPERTÓ! El salario que intentan meter es: %', NEW.salary;
+    SELECT max_salary,
+           min_salary
+    INTO
+    max_salary_job,
+    min_salary_job
+    FROM jobs
+    WHERE job_id = NEW.job_id;
+
+    RAISE NOTICE '--- DEBUGGING TRIGGER ---';
+    RAISE NOTICE 'Salario intentado: %', NEW.salary;
+    RAISE NOTICE 'Límite Mínimo: %', min_salary_job;
+    RAISE NOTICE 'Límite Máximo: %', max_salary_job;
+    RAISE NOTICE '-------------------------';
+
+    IF (min_salary_job <= NEW.salary AND NEW.salary <= max_salary_job) THEN
+        RETURN NEW;
+    ELSE
+        RAISE EXCEPTION 'Salario Invalido. Debe estar entre % y %', min_salary_job, max_salary_job;
+    end if;
+
+END;
+$$
+    LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER vigilar_salarios
+    BEFORE INSERT OR UPDATE
+    ON employees
+    FOR EACH ROW
+EXECUTE PROCEDURE verificar_salario();
